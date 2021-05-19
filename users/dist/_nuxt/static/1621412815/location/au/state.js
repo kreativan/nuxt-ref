385041c6e6006a6678a212d9045b6ebf55010be2
Ivan Milincic <kreativan@outlook.com>
2021-05-19 10:50:27 +0200

    /users generate

--- users/dist/_nuxt/static/1621412815/location/au/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-ref\u002Fusers\u002Fdist\u002F_nuxt\u002Fstatic\u002F1621412815",layout:"default",error:a,serverRendered:true,routePath:"\u002Flocation\u002Fau",config:{_app:{basePath:"\u002Fnuxt-ref\u002Fusers\u002Fdist\u002F",assetsPath:"\u002Fnuxt-ref\u002Fusers\u002Fdist\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ users/dist/_nuxt/static/1621414201/location/ie/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-ref\u002Fusers\u002Fdist\u002F_nuxt\u002Fstatic\u002F1621414201",layout:"default",error:a,serverRendered:true,routePath:"\u002Flocation\u002Fie",config:{_app:{basePath:"\u002Fnuxt-ref\u002Fusers\u002Fdist\u002F",assetsPath:"\u002Fnuxt-ref\u002Fusers\u002Fdist\u002F_nuxt\u002F",cdnURL:a}}}}(null));
